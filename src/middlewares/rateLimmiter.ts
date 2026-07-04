0e1c6a46e106b34599af70e92dc632dae6928773
Aakash Jaiswal <aakashjaiswal@Aakashs-MacBook-Air.local>
2026-07-05 03:45:33 +0530

    Build sliding window rate limitter

--- src/middlewares/rateLimmiter.ts
+++ src/middlewares/rateLimmiter.ts
@@ -4,7 +4,7 @@ import { Redis } from "ioredis";
 import {config} from '../library/config.js'
 
 const MAX_LIMIT = 5;
-const REFRESH_TIME = 10; //in seconds
+const REFRESH_TIME = 30; //in seconds
 
 export default class RateLimiter {
     private client: Redis;
@@ -42,7 +42,7 @@ export default class RateLimiter {
 
             if (currentCount > MAX_LIMIT) {
                 return res.status(429).json({
-                    message: "TOO MANY REQUEST -"
+                    message: "TOO MANY REQUEST"
                 })
             }
 
@@ -50,7 +50,38 @@ export default class RateLimiter {
             next();
         }
         catch (err) {
-            next(err);
+            next();
+        }
+    }
+
+
+    async slidingRateLimit(req: Request, res: Response, next: NextFunction){
+        try {
+
+            const ip = req.ip;
+            const currentTime = Date.now();
+
+            const key = `sliding-limit:${ip}`
+
+            const timeStamps = await this.client.lrange(key,0,-1);
+            const currentWindow = timeStamps.filter(t=> (currentTime - Number(t)) <= REFRESH_TIME)
+
+            if (currentWindow.length > MAX_LIMIT){
+                return res.status(429).json({
+                    message: "TOO MANY REQUEST"
+                })
+            }
+
+            await this.client.lpush(key,currentTime);
+            await this.client.ltrim(key, 0, MAX_LIMIT -1)
+            
+            next();
+
+
+
+
+        } catch(err){
+            next()
         }
     }
 
